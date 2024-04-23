b0e661c88ed34a71f04c893cd06507390b008429
Azizbek Isroilov <aziz16110904@gmail.com>
2024-04-23 09:33:12 +0500

    wrote new admin route starter

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware('admin')->group(function () {
+    Route::get('/admin', function () {
+        return view('admin');
+    })->name('admin');
+});
+
+require __DIR__ . '/auth.php';
